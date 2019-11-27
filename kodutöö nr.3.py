152705298ba2bbce55d2b71bc323deb89a0097f3
Kerdo482 <56673744+Kerdo482@users.noreply.github.com>
2019-11-27 12:05:05 +0200

    Add files via upload

---
+++ kodutöö nr.3.py
@@ -0,0 +1,13 @@
+x, y, z = input("kolm numbrit: ").split(",")
+x = float(x)
+y = float(y)
+z = float(z)
+if x == y and z == y:
+    korrutis = x * y * z
+    print (korrutis)
+else:
+    summa = x + y + z
+    print (summa)
+    
+
+
