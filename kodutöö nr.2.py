ada2195acaa0010d906dc9ac1e494e886b46943a
Kerdo482 <56673744+Kerdo482@users.noreply.github.com>
2019-11-27 12:06:35 +0200

    Add files via upload

---
+++ kodutöö nr.2.py
@@ -0,0 +1,18 @@
+num = int(input("Sisend: "))
+if num == 6 or num == 8 or num == 10 or num == 12 or num == 14 or num == 16 or num == 18 or num == 20: 
+    print("Valjund: Ei sobi")
+elif num % 2 == 0:
+    print("Valjund:Sobib")
+else:
+    print("Ei Sobi")
+    
+
+    
+
+    
+   
+    
+
+    
+
+
